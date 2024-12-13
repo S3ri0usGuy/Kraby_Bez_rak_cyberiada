1ae2a1df153ca8332691857fc9f34c593acb7b65
S3ri0usGuy <skx88craft@gmail.com>
2024-12-13 03:39:28 +0100

    hehe

--- Assets/AI/Enemy V2/StateMachine/ConcreteStates/EnemyIdleState.cs
+++ Assets/AI/Enemy V2/StateMachine/ConcreteStates/EnemyIdleState.cs
@@ -37,24 +37,16 @@ public class EnemyIdleState : EnemyState
             enemy.stateMachine.ChangeState(enemy.chaseState);
         }// jeœli nie jest agro bêdzie ³azi³ w losowe miejsca pathfinding do zajebania ale to bardziej w chase,
 
-<<<<<<< Updated upstream
         /*_direction = (_targetPos - enemy.transform.position).normalized;
-=======
-        _direction = (_targetPos - enemy.transform.position).normalized;
         _direction.y = 0;
->>>>>>> Stashed changes
 
         enemy.MoveEnemy(_direction * enemy.randomMovementSpeed);
         if ((enemy.transform.position - _targetPos).sqrMagnitude < 0.01f)
         {
             _targetPos = GetRandomPointInSphere();
             Debug.Log("Enemy : changed pos, destination reached");
-<<<<<<< Updated upstream
         }*/
-=======
-        }
 
->>>>>>> Stashed changes
     }
 
     public override void PhiscsUpdate()
@@ -63,11 +55,6 @@ public class EnemyIdleState : EnemyState
     }
     /*private Vector3 GetRandomPointInSphere()
     {
-<<<<<<< Updated upstream
         return enemy.transform.position + (Vector3)UnityEngine.Random.insideUnitSphere * enemy.randomMovementRange;
     }*/
-=======
-        return  (Vector3)UnityEngine.Random.insideUnitCircle * enemy.randomMovementRange;
-    }
->>>>>>> Stashed changes
 }
