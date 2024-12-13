a0f276d6f79db4a04c988870c8a9a99a2c02592d
romandykyi <roman.dykyi.8210@gmail.com>
2024-12-13 16:06:45 +0100

    Fix audio and healthbar

--- Assets/Scripts/Player/Health bar.cs
+++ Assets/Scripts/Player/Health bar.cs
@@ -28,26 +28,6 @@ public class Healthbar : MonoBehaviour
         if (healthBarImage != null)
         {
             healthBarImage.fillAmount = (float)damagable.Hp / damagable.MaxHp;
-            UpdateHealthBarColor(damagable.Hp);
-        }
-    }
-
-    private void UpdateHealthBarColor(int currentHp)
-    {
-        if (healthBarImage != null)
-        {
-            if (currentHp > damagable.MaxHp * 0.5f)
-            {
-                healthBarImage.color = Color.green; // Zielony
-            }
-            else if (currentHp > damagable.MaxHp * 0.2f)
-            {
-                healthBarImage.color = Color.yellow; // Żółty
-            }
-            else
-            {
-                healthBarImage.color = Color.red; // Czerwony
-            }
         }
     }
 }
